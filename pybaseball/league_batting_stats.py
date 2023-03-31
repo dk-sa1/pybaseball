6b94ede5bd1910c9d31c991d660de77f165a8829
dk-sa1 <m.salinas124@yahoo.com>
2023-03-31 14:24:05 -0500

    temp

--- pybaseball/league_batting_stats.py
+++ pybaseball/league_batting_stats.py
@@ -15,8 +15,11 @@ def get_soup(start_dt: date, end_dt: date) -> BeautifulSoup:
     #    print('Error: a date range needs to be specified')
     #    return None
     url = "http://www.baseball-reference.com/leagues/daily.cgi?user_team=&bust_cache=&type=b&lastndays=7&dates=fromandto&fromandto={}.{}&level=mlb&franch=&stat=&stat_value=0".format(start_dt, end_dt)
-    s = requests.get(url).content
-    return BeautifulSoup(s, "html.parser")
+
+    s = session.get(url).content
+    # a workaround to avoid beautiful soup applying the wrong encoding
+    s = str(s).encode()
+    return BeautifulSoup(s, features="lxml")
 
 
 def get_table(soup: BeautifulSoup) -> pd.DataFrame:
